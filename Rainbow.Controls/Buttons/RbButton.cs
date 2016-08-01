e066f674617a57af805f4d18defbfeca9880380f
BigerXie <xiekitt@163.com>
2016-08-01 23:15:56 +0800

    Rainbow Controls Update

---
+++ Rainbow.Controls/Buttons/RbButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Rainbow.Controls.Buttons
+{
+    public class RbButton : Button
+    {
+        static RbButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(RbButton), new FrameworkPropertyMetadata(typeof(RbButton)));
+        }
+    }
+}
---
+++ Rainbow.Controls/Buttons/RbIconButton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Rainbow.Controls.Buttons
+{
+    public class RbIconButton : Control
+    {
+        static RbIconButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(RbIconButton), new FrameworkPropertyMetadata(typeof(RbIconButton)));
+        }
+    }
+}
