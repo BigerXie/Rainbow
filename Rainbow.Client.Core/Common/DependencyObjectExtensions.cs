32586775c9d134bbc3d59dd9a82649ca8f0546e9
xieqq-PC\xieqq <875645729@qq.com>
2017-08-07 17:43:53 +0800

    xieqq

---
+++ Rainbow.Client.Core/Common/DependencyObjectExtensions.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Rainbow.UI.Core.Common
+{
+	public static class DependencyObjectExtensions
+	{
+		/// <summary>
+		/// Finds immediate parent of the child control
+		/// </summary>
+		/// <typeparam name="T">Finds specific Type of parent control</typeparam>
+		/// <param name="child">Child control in use</param>
+		/// <returns></returns>
+		public static T FindParent<T>(this DependencyObject child) where T : DependencyObject
+		{
+			//get parent item
+			DependencyObject parentObject = VisualTreeHelper.GetParent(child);
+
+			//we've reached the end of the tree
+			if (parentObject == null) return null;
+
+			//check if the parent matches the type we're looking for
+			var parent = parentObject as T;
+			return parent ?? FindParent<T>(parentObject);
+		}
+
+		/// <summary>
+		/// Finds child of specific type of specific name
+		/// </summary>
+		/// <typeparam name="T">Type of child</typeparam>
+		/// <param name="parent">Current parent control</param>
+		/// <param name="childName">Name of the child control to be found</param>
+		/// <returns></returns>
+		public static T FindChild<T>(this DependencyObject parent, string childName) where T : DependencyObject
+		{
+			// Confirm parent and childName are valid.  
+			if (parent == null) return null;
+			T foundChild = null;
+			int childrenCount = VisualTreeHelper.GetChildrenCount(parent);
+			for (int i = 0; i < childrenCount; i++)
+			{
+				DependencyObject child = VisualTreeHelper.GetChild(parent, i);
+
+				// If the child is not of the request child type child  
+				var childType = child as T;
+				if (childType == null)
+				{
+					// recursively drill down the tree  
+					foundChild = FindChild<T>(child, childName);
+					// If the child is found, break so we do not overwrite the found child.    
+					if (foundChild != null) break;
+				}
+				else if (!String.IsNullOrEmpty(childName))
+				{
+					var frameworkElement = child as FrameworkElement;
+					// If the child's name is set for search    
+					if (frameworkElement != null && frameworkElement.Name == childName)
+					{
+						// if the child's name is of the request name  
+						foundChild = (T)child;
+						break;
+					}
+				}
+				else
+				{
+					// child control found.
+					foundChild = (T)child;
+					break;
+				}
+			}
+			return foundChild;
+		}
+
+		/// <summary>
+		/// Finds child of specific type of specific name
+		/// </summary>
+		/// <typeparam name="T">Type of child</typeparam>
+		/// <param name="parent">Current parent control</param>
+		/// <returns></returns>
+		public static T FindChild<T>(this DependencyObject parent) where T : DependencyObject
+		{
+			// Confirm parent and childName are valid.  
+			if (parent == null) return null;
+			T foundChild = null;
+			int childrenCount = VisualTreeHelper.GetChildrenCount(parent);
+			for (int i = 0; i < childrenCount; i++)
+			{
+				DependencyObject child = VisualTreeHelper.GetChild(parent, i);
+
+				// If the child is not of the request child type child  
+				var childType = child as T;
+				if (childType == null)
+				{
+					// recursively drill down the tree  
+					foundChild = FindChild<T>(child);
+					// If the child is found, break so we do not overwrite the found child.    
+					if (foundChild != null) break;
+				}
+				else
+				{
+					// child control found.
+					foundChild = (T)child;
+					break;
+				}
+			}
+			return foundChild;
+		}
+
+		/// <summary>
+		/// Get collection of child controls of specific types
+		/// </summary>
+		/// <typeparam name="T">Type of controls to be fetched</typeparam>
+		/// <param name="parent">Current parent</param>
+		/// <returns></returns>
+		public static IList<T> GetVisualChildCollection<T>(this DependencyObject parent) where T : DependencyObject
+		{
+			var visualCollection = new List<T>();
+			GetVisualChildCollection(parent, visualCollection);
+			return visualCollection;
+		}
+
+		private static void GetVisualChildCollection<T>(DependencyObject parent, IList<T> visualCollection)
+			where T : DependencyObject
+		{
+			int count = VisualTreeHelper.GetChildrenCount(parent);
+			for (int i = 0; i < count; i++)
+			{
+				DependencyObject child = VisualTreeHelper.GetChild(parent, i);
+				if (child is T)
+				{
+					visualCollection.Add(child as T);
+				}
+				else if (child != null)
+				{
+					GetVisualChildCollection(child, visualCollection);
+				}
+			}
+		}
+
+	    public static T GetFirstVisualChild<T>(DependencyObject depObj) where T : DependencyObject
+	    {
+	        if (depObj != null)
+	        {
+	            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+	            {
+	                DependencyObject child = VisualTreeHelper.GetChild(depObj, i);
+	                if (child is T)
+	                {
+	                    return (T)child;
+	                }
+
+	                T childItem = GetFirstVisualChild<T>(child);
+	                if (childItem != null)
+	                {
+	                    return childItem;
+	                }
+	            }
+	        }
+
+	        return null;
+	    }
+	}
+}
